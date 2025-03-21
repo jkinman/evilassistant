f50235a23e081dff45a6c32e881b1470d7f14bb6
Joel Kinman <joel.kinman@gmail.com>
2025-03-20 19:38:54 -0700

    Update config.py

--- evilassistant/config.py
+++ evilassistant/config.py
@@ -5,7 +5,8 @@ CHANNELS = 1
 SILENCE_THRESHOLD = 500
 SILENCE_DURATION = 1.5
 
-WAKE_PHRASES = ["evil assistant", "i summon you", "dark one", "cthulhu"]  # List of wake-up phrases
+WAKE_PHRASES = ["evil assistant", "i summon you", "dark one", "cthulhu", "amanda"]  # List of wake-up phrases
+FOLLOW_UP_PROMPT = "What else do you seek, mortal?"
 PIPER_MODEL = "en_US-lessac-low.onnx"
 PIPER_CONFIG = "en_US-lessac-low.onnx.json"
 SOX_EFFECTS = "tempo 0.85 pitch -600 gain -4 overdrive 6"
