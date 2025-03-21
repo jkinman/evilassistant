b30cdfa14861dc20d3b0bd21ee8f659e4faba287
Joel Kinman <joel.kinman@gmail.com>
2025-03-20 19:14:50 -0700

    Update config.py

--- evilassistant/config.py
+++ evilassistant/config.py
@@ -8,6 +8,6 @@ SILENCE_DURATION = 1.5
 WAKE_PHRASE = "evil assistant"
 PIPER_MODEL = "en_US-lessac-low.onnx"
 PIPER_CONFIG = "en_US-lessac-low.onnx.json"
-SOX_EFFECTS = "tempo 0.85 pitch -700 gain -5 overdrive 10"
+SOX_EFFECTS = "tempo 0.85 pitch -600 gain -4 overdrive 6"
 # SOX_EFFECTS = "tempo 0.7 pitch -500 reverb 60 overdrive 10 gain -5 tremolo 2 30"
 
