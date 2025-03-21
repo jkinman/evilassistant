ad126acb27baa7709939f214155248dd40bf4770
Joel Kinman <joel.kinman@gmail.com>
2025-03-20 20:46:05 -0700

    Update config.py

--- evilassistant/config.py
+++ evilassistant/config.py
@@ -5,7 +5,8 @@ CHANNELS = 1
 SILENCE_THRESHOLD = 500
 SILENCE_DURATION = 1.5
 
-WAKE_PHRASES = ["evil assistant", "i summon you", "dark one", "cthulhu", "durans dick"]  # Added new phrase
+WAKE_PHRASES = ["evil assistant", "i summon you", "dark one", "cthulhu", "durans dick"]  # Added "durans dick"
+STOP_PHRASES = ["shut up", "be silent", "stop", "unsummon"]  # Added "stop" and "unsummon"
 FOLLOW_UP_PROMPT = "What else do you seek, mortal?"
 PIPER_MODEL = "en_US-lessac-low.onnx"
 PIPER_CONFIG = "en_US-lessac-low.onnx.json"
