8c108a78405fc32b38c267c979be3f78bcde1ffe
Joel Kinman <joel.kinman@gmail.com>
2025-03-20 17:59:00 -0700

    Create config.py

---
+++ evilassistant/config.py
@@ -0,0 +1,12 @@
+RATE = 44100
+CHUNK_DURATION = 3
+CHANNELS = 1
+SILENCE_THRESHOLD = 500
+SILENCE_DURATION = 1.5
+
+WAKE_PHRASE = "evil assistant"
+WELCOME_PHRASE = "I am Evil Assistant, what dark query do you dare to ask?"
+
+PIPER_MODEL = "en_US-lessac-low.onnx"
+PIPER_CONFIG = "en_US-lessac-low.onnx.json"
+SOX_EFFECTS = "tempo 0.8 pitch -300 reverb 50 overdrive 10"
